5d732f4cdbe5d35b608ee09957b303cacb7fd0d4
sheep <82494123+thejanjan@users.noreply.github.com>
2022-05-04 18:18:13 -0700

    LevelManager: Change some casing

--- level/LevelManager.gd
+++ level/LevelManager.gd
@@ -9,22 +9,23 @@ A Level is a high-order scene node that generates an entire level within it.
 """
 
 # Create the helper nodes for the LevelManager.
-var _levelGenerator = null
+var _level_generator = null
 var _level = null
+var _level_dictionary = null
 
 
 func _ready():
 	# Create our LevelGenerator node.
-	self._levelGenerator = preload("res://level/LevelGenerator.tscn").instance()
-	self.add_child(_levelGenerator)
+	_level_generator = preload("res://level/LevelGenerator.tscn").instance()
+	self.add_child(_level_generator)
 	
 	# Load a test tileset.
 	# TODO - refactor to an external class.
-	self._level = preload("res://level/templates/Level_Headphones.tscn").instance()
-	self._levelGenerator.add_level(self._level)
+	_level = preload("res://level/templates/Level_Headphones.tscn").instance()
+	self._level_generator.add_level(_level)
 	
 	# Build the test tileset.
-	self._levelGenerator.build_level()
+	self._level_generator.build_level()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
